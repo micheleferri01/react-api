3ee99343976ea9f30b9f435e934c7306bb89e5bc
micheleferri01 <micheleferri095@gmail.com>
2026-02-13 15:40:06 +0100

    stampata in console la lista delle attrici

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import axios from "axios"
+import { useState, useEffect } from "react"
 
+const baseUrl = 'https://lanciweb.github.io/demo/api';
 
 export default function App() {
+  const [actors, setActors] = useState([])
+  
+  const getActresses = (apiUrl = baseUrl + '/actresses') => {
+    axios.get(apiUrl).then( (res) => {
+      console.log(res.data);
+    })
+  }
 
+  useEffect(() => {
+    getActresses();
+  }, [])
+  
   return (
     <>
       
