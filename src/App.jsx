3bb18621890f8e66d146eea0e66b5cc3aa114de5
micheleferri01 <micheleferri095@gmail.com>
2026-02-13 17:11:40 +0100

    generata lista degli attori

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,18 @@ import { useState, useEffect } from "react"
 const baseUrl = 'https://lanciweb.github.io/demo/api';
 
 export default function App() {
+  const [actresses,setActresses] = useState([])
   const [actors, setActors] = useState([])
   
   const getActresses = (apiUrl = baseUrl + '/actresses') => {
     axios.get(apiUrl).then( (res) => {
+      console.log(res.data);
+      setActresses(res.data);
+    })
+  }
+
+  const getActors = (apiUrl = baseUrl + '/actors') => {
+    axios.get(apiUrl).then((res) => {
       console.log(res.data);
       setActors(res.data);
     })
@@ -15,6 +23,7 @@ export default function App() {
 
   useEffect(() => {
     getActresses();
+    getActors()
   }, [])
 
   return (
@@ -25,7 +34,7 @@ export default function App() {
       <div className="container">
         <div className="row g-3">
           {
-            actors.map((actor) =>
+            actresses.map((actor) =>
             <div key={actor.id} className="col-4 rounded">
               <div className="custom-card h-100">
                 <img src={actor.image} alt={actor.name} className="card-image"/>
@@ -45,6 +54,31 @@ export default function App() {
         </div>
       </div>
 
+      <div className="py-4">
+        <h1 className='text-center'>Actors</h1>
+      </div>
+      <div className="container">
+        <div className="row g-3">
+          {
+            actors.map((actor) =>
+              <div key={actor.id} className="col-4 rounded">
+                <div className="custom-card h-100">
+                  <img src={actor.image} alt={actor.name} className="card-image" />
+                  <div className="card-body p-2">
+                    <h2 className="card-title pb-3">{actor.name}</h2>
+                    <ul className="card-text list-unstyled">
+                      <li><span className="fw-bold">Birth date:</span> {actor.birth_year}</li>
+                      <li><span className="fw-bold">Nationality:</span> {actor.nationality}</li>
+                      <li><span className="fw-bold">Biography:</span> {actor.biography}</li>
+                      <li><span className="fw-bold">Awards:</span> {actor.awards} </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            )
+          }
+        </div>
+      </div>
     </>
   )
 }
